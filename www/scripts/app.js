1c397122cb304c723738f3c665301106404e1d50
Prosenjit Manna <prosenjit@itobuz.com>
2016-07-12 10:54:58 +0530

    ui router added

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -2,7 +2,24 @@
 Main script
 */
 'use strict';
-var app = angular.module('app', [])
+var app = angular.module('app', ['ui.router'])
 	.run([function(){
 		console.log('app is running.')
 	}]);
+
+	app.config(function($stateProvider, $urlRouterProvider) {
+	  //
+	  // For any unmatched url, redirect to /state1
+	  $urlRouterProvider.otherwise("/state1");
+	  //
+	  // Now set up the states
+	  $stateProvider
+	    .state('state1', {
+	      url: "/state1",
+	      templateUrl: "views/state-1.html"
+	    })
+	    .state('state2', {
+	      url: "/state2",
+	      templateUrl: "views/state-2.html"
+	    })
+	});
